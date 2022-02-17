69745b30b5b22f6eb7bd237a278b160190882d9f
Alexandrhall <alexander.hall@medieinstitutet.se>
2022-02-17 20:59:49 +0100

    update

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const database = require("./database.js");
-const { ObjectId } = require("mongodb");
-const middleWare = require("./utils.js");
 
 const checkedRoutes = require("./routes/checked-routes.js");
 const editRoutes = require("./routes/edit-routes.js");
 const delRoutes = require("./routes/del-routes.js");
 const showRoutes = require("./routes/show-routes.js");
 const sortRoutes = require("./routes/sort-routes.js");
+const homeRoutes = require("./routes/home-routes.js");
 
 const app = express();
 
@@ -25,32 +23,7 @@ app.set("view engine", "hbs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.get(
-    "/",
-    async (req, res, next) => {
-        middleWare(req, res, next);
-    },
-    async (req, res, next) => {
-        const tasks = await database.getTodo();
-        res.render("home", { tasks });
-    }
-);
-
-app.post("/", async (req, res) => {
-    let dateNow = JSON.stringify(new Date());
-
-    if (req.body.description != "") {
-        const newTask = {
-            created: dateNow,
-            description: req.body.description,
-            done: false,
-        };
-        const db = await database.getDb();
-        await db.collection("tasks").insertOne(newTask);
-    }
-
-    res.redirect("/");
-});
+app.use("/", homeRoutes);
 
 app.use("/checked", checkedRoutes);
 
---
+++ routes/home-routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const database = require("../database.js");
+const middleWare = require("../utils.js");
+
+const router = express.Router();
+
+router.get(
+    "/",
+    async (req, res, next) => {
+        middleWare(req, res, next);
+    },
+    async (req, res, next) => {
+        const tasks = await database.getTodo();
+        res.render("home", { tasks });
+    }
+);
+
+router.post("/", async (req, res) => {
+    let dateNow = JSON.stringify(new Date());
+
+    if (req.body.description != "") {
+        const newTask = {
+            created: dateNow,
+            description: req.body.description,
+            done: false,
+        };
+        const db = await database.getDb();
+        await db.collection("tasks").insertOne(newTask);
+    }
+
+    res.redirect("/");
+});
+
+module.exports = router;
